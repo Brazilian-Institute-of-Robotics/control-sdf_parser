8f312ae7282b1b40ac4da419f878bfe9efda2ac2
Gustavo Neves <gustavo.neves12@gmail.com>
2015-03-19 15:43:12 -0300

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,20 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+#include <iostream>
+
+namespace sdf_parser
+{
+	class DummyClass
+	{
+		public: 
+			/**
+			* Print a welcome to stdout
+			* \return nothing
+			*/
+			void welcome();
+	};
+
+} // end namespace sdf_parser
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <sdf_parser/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+	sdf_parser::DummyClass dummyClass;
+	dummyClass.welcome();
+
+	return 0;
+}
